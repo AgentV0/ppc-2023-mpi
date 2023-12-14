06cbfbd9f98a8ed6c6bbd6074cb87196b16db64f
AgentV0 <Vlad58009@gmail.com>
2023-12-14 17:39:07 +0300

    lab2_fix2

--- tasks/task_2/ryabkov_v_vertical_strip/strip_vertical_scheme.h
+++ tasks/task_2/ryabkov_v_vertical_strip/strip_vertical_scheme.h
@@ -3,6 +3,8 @@
 #ifndef TASKS_TASK_2_RYABKOV_V_VERTICAL_STRIP_STRIP_VERTICAL_SCHEME_H_
 #define TASKS_TASK_2_RYABKOV_V_VERTICAL_STRIP_STRIP_VERTICAL_SCHEME_H_
 
+#include <algorithm>
+
 int* init_empty_matrix(int rows);
 int* get_random_matrix(int rows, int columns);
 void sequential_matrix_multiplication(const int* A, const int A_rows,
